5e14ed9c9175d2015c6ae950037317d40e4ad2ea
Shivansh2004-Mishra <shivanshmishra0029@gmail.com>
2025-05-17 19:51:46 +0530

    update localhost

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:0.0.0.0/todo");
+mongoose.connect("http://localhost:27017/todo");
 const trySchema = new mongoose.Schema({
   name: String
 });
